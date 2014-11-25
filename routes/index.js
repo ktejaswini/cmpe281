0bfb1fe9e5ae9ebdb0f297fa787d3d87fa9a6a4a
ktejaswini <tejaswini.reddy91@gmail.com>
2014-11-25 15:37:49 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,13 @@
 var express = require('express');
 var http = require('http');
-var router = express.Router();
+var Client = require('node-rest-client').Client;
 
-// Homepage
-router.get('/', function(req, res) {
-	var output = client.get("http://life.cfapps.io/api/gumball/",function(response) {
-		response.on('data', function (chunck) {
-			console.log(chunck);
-			res.render('index', { title:chunck });
-		});
-	
-	
-	});
-	output.end();
-});
-//{
-//host : 'localhost',
-//port : 8080,
-//path : 'http://life.cfapps.io/api/gumball/',
-//method : 'GET' 
-//};
-//var output = http.request(optionsget, function(response) {
-//response.on('data', function (chunck) {
-	//console.log(chunck);
-	//res.render('index', { title:chunck });
-//	});
+client = new Client();
 
-//Turn the crank
-router.get('/turn', function(req, res) {
-	var output = client.put("http://life.cfapps.io/api/gumball/turn",function(response) {
-		response.on('data', function (chunck) {
-			console.log(chunck);
-			res.render('index', { title:chunck });
-		});
-	
-	
-	});
-	output.end();
-});
-	
-	/*var optionsget = {
-    	//host : 'localhost',
-    	//port : 8080,
-    	path : 'http://life.cfapps.io/api/gumball/turn',
-    	method : 'PUT' 
-	};
-	var output = http.request(optionsget, function(response) {
-		response.on('data', function (chunck) {
-			console.log(chunck);
-			res.render('index', { title: chunck });
-		});
-	});
-	output.end();
-});*/
 
-//Insert a quarter
-router.get('/insrt', function(req, res) {
-	
-	var output = client.put("http://life.cfapps.io/api/gumball/turn",function(response) {
-		response.on('data', function (chunck) {
-			console.log(chunck);
-			res.render('index', { title:chunck });
-		});
-	
-	
-	});
-	output.end();
-});
-	
-/*var optionsget = {
-    	//host : 'localhost',
-    	//port : 8080,
-    	path : 'http://life.cfapps.io/api/gumball/insrt',
-    	method : 'PUT' 
-	};
-	var output = http.request(optionsget, function(response) {
-		response.on('data', function (chunck) {
-			console.log(chunck);
-			res.render('index', { title: chunck });
-		});
-	});
-	output.end();
-});*/
-
-module.exports = router;
+client.get("http://life.cfapps.io/api/gumball", function(data, response){
+            // parsed response body as js object
+            console.log(data);
+            // raw response
+            console.log(response);
+        });
