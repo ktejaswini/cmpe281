18758bfd8d7bda8baa644af3ffb9be3ea5aaadbb
ktejaswini <tejaswini.reddy91@gmail.com>
2014-11-25 15:23:12 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,45 @@ var router = express.Router();
 
 // Homepage
 router.get('/', function(req, res) {
-	var optionsget = {
-    	host : 'localhost',
-    	port : 8080,
-    	path : '/GrailsGumballMachineVer1/api/gumball/',
-    	method : 'GET' 
-	};
-	var output = http.request(optionsget, function(response) {
+	var output = client.get("http://life.cfapps.io/api/gumball/",function(response) {
 		response.on('data', function (chunck) {
 			console.log(chunck);
 			res.render('index', { title:chunck });
 		});
+	
+	
 	});
 	output.end();
 });
+//{
+//host : 'localhost',
+//port : 8080,
+//path : 'http://life.cfapps.io/api/gumball/',
+//method : 'GET' 
+//};
+//var output = http.request(optionsget, function(response) {
+//response.on('data', function (chunck) {
+	//console.log(chunck);
+	//res.render('index', { title:chunck });
+//	});
 
 //Turn the crank
 router.get('/turn', function(req, res) {
-	var optionsget = {
-    	host : 'localhost',
-    	port : 8080,
-    	path : '/GrailsGumballMachineVer1/api/gumball/turn',
+	var output = client.put("http://life.cfapps.io/api/gumball/turn",function(response) {
+		response.on('data', function (chunck) {
+			console.log(chunck);
+			res.render('index', { title:chunck });
+		});
+	
+	
+	});
+	output.end();
+});
+	
+	/*var optionsget = {
+    	//host : 'localhost',
+    	//port : 8080,
+    	path : 'http://life.cfapps.io/api/gumball/turn',
     	method : 'PUT' 
 	};
 	var output = http.request(optionsget, function(response) {
@@ -34,14 +52,26 @@ router.get('/turn', function(req, res) {
 		});
 	});
 	output.end();
-});
+});*/
 
 //Insert a quarter
 router.get('/insrt', function(req, res) {
-	var optionsget = {
-    	host : 'localhost',
-    	port : 8080,
-    	path : '/GrailsGumballMachineVer1/api/gumball/insrt',
+	
+	var output = client.put("http://life.cfapps.io/api/gumball/turn",function(response) {
+		response.on('data', function (chunck) {
+			console.log(chunck);
+			res.render('index', { title:chunck });
+		});
+	
+	
+	});
+	output.end();
+});
+	
+/*var optionsget = {
+    	//host : 'localhost',
+    	//port : 8080,
+    	path : 'http://life.cfapps.io/api/gumball/insrt',
     	method : 'PUT' 
 	};
 	var output = http.request(optionsget, function(response) {
@@ -51,6 +81,6 @@ router.get('/insrt', function(req, res) {
 		});
 	});
 	output.end();
-});
+});*/
 
 module.exports = router;
