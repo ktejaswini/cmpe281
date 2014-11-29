ddc87d7add38f7f2169ad3128e494ca0885cc781
ktejaswini <tejaswini.reddy91@gmail.com>
2014-11-28 23:05:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
-
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
